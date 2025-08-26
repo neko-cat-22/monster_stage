f089a00a73a1f20cbf2d84be37f6bd099a2eff5f
neko-cat-22 <notbadcat@icloud.com>
2025-08-27 02:00:55 +0900

    Create quests.js

---
+++ quests.js
@@ -0,0 +1,26 @@
+const quests = [
+  {
+    category: "超絶",
+    name: "カナン",
+    required: ["重力バリア"],
+    appearance: ["ダメージウォール", "ワープ"],
+    stage: ["光属性", "ボス:カナン"],
+    attribute: "光"
+  },
+  {
+    category: "轟絶",
+    name: "モラル",
+    required: ["ダメージウォール", "魔法陣"],
+    appearance: ["重力バリア"],
+    stage: ["火属性", "ボス:モラル"],
+    attribute: "火"
+  },
+  {
+    category: "爆絶",
+    name: "ラグナロク",
+    required: ["ワープ"],
+    appearance: ["地雷", "ブロック"],
+    stage: ["木属性", "ボス:ラグナロク"],
+    attribute: "木"
+  }
+];
